ca27d2d5ba3ef9ee5df9eaf5a946174614343a00
Alban Tiberghien <atiberghien@numnprod.fr>
2023-10-21 14:08:26 +0200

    Update build scripts

--- build_pc.sh
+++ build_pc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker build --build-arg="STAGE=standalone" -t mrc_backend:latest mrc-backend
+docker build --build-arg="STAGE=standalone" -t mrc_mosquitto:latest -f mrc-hardware/Dockerfile.mosquitto mrc-hardware
 docker build --build-arg="STAGE=standalone" -t mrc_printer:latest -f mrc-hardware/Dockerfile.printer mrc-hardware
 docker build --build-arg="STAGE=standalone" -t mrc_nginx:latest mrc-nginx
 docker build --build-arg="STAGE=standalone" -t mrc_frontend:latest mrc-frontend
--- build_web.sh
+++ build_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BACK=0.26
-FRONT=0.17
+BACK=0.28
+FRONT=0.20
 NGINX=0.7
 
 docker build --build-arg="STAGE=web" -t mrc_web_backend:${BACK} mrc-backend
