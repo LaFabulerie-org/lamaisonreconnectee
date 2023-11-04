d4cee6d373c15dca4de87da321d85bf2e4584b72
Alban Tiberghien <atiberghien@numnprod.fr>
2023-11-04 16:42:38 +0100

    Update deployment stuff

--- update-rasp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose-rasp.yml stop
-docker container prune -f
-git pull origin master
-git submodule foreach 'git pull origin master'
-docker-compose -f docker-compose-rasp.yml build
-docker-compose -f docker-compose-rasp.yml up -d
