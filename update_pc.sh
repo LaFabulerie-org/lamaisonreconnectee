6c7ed8be0d2499791c4323dc8474734b37abb3e8
Mindiell <mindiell@mindiell.net>
2024-02-29 10:05:10 +0100

    Renommage du fichier de mise à jour pour plus de cohérence

--- update_pc.sh
+++ update_pc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker compose -f docker-compose.yml down
-git pull origin master
-git submodule foreach 'git pull origin master'
+git pull origin main
+git submodule foreach 'git pull origin main'
 ./build_pc.sh
 docker compose -f docker-compose.yml up -d
